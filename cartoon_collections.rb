5fef3a41fea3541ce5eabe7a64951b43a2dadb32
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-02 10:40:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect { |cheese, value| value == cheese_types }
+  array.detect { |cheese, value| value == cheese_types; p value }
 end
