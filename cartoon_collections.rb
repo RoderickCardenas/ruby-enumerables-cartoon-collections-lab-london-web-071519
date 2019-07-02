fc889292cc8415b9050b56497326cabc9dee9d24
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-02 10:18:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |word| word.length >= 4 }
+  array.any? { |word| if word.length >= 4; p true; else p false }
 end
 
 def find_the_cheese# code an argument here
